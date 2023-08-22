050b5f58423e907bc5e7a1155d0e12343b55f4fc
indigoBill <117851497+indigoBill@users.noreply.github.com>
2023-08-22 12:27:46 -0400

    Add event listener to play game using buttons

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//CREATE REFERENCE TO BUTTON ELEMENTS IN HTML 
+let buttons = document.querySelectorAll(".player-select");
+
+//DECLARE VARIABLES FOR CURRENT SCORES FOR THE GAME
+let currPlayScore = 0;
+let currCompScore = 0;
+
 //GET COMPUTER CHOICE
-function getComputerChoice(){
+function computerSelection(){
     let num = Math.ceil(Math.random() * 3);
 
     if(num === 1){
@@ -11,58 +18,12 @@ function getComputerChoice(){
     } 
 }
 
-//GET PLAYER CHOICE THROUGH A PROMPT & MAKE IT CASE-INSENSITIVE
-function playerSelection(){
-    let playerSelection = prompt("ROCK,PAPER, OR SCISSORS?");
-    let playerCompare = playerSelection.toUpperCase();
-    return playerCompare;
-}
+//GET PLAYER CHOICE FROM CLICK EVENT
+function playerSelection(event){
+    let selectedButton = event.target.textContent;
+    let playerSelection = selectedButton.toUpperCase();
 
-//PLAY A SINGLE ROUND OF THE GAME
-function playRound(playerSelection, getComputerChoice){
-
-    if(playerSelection === "ROCK"){
-        if(getComputerChoice === "PAPER"){
-            result(playerSelection, getComputerChoice, 0);
-            return 0;
-        }else if(getComputerChoice === "SCISSORS"){
-            result(playerSelection, getComputerChoice, 1);
-            return 1;
-        }else{
-            result(playerSelection, getComputerChoice);
-        }
-    } else if(playerSelection === "PAPER"){
-        if(getComputerChoice === "ROCK"){
-            result(playerSelection, getComputerChoice, 1);
-            return 1;
-        }else if(getComputerChoice === "SCISSORS"){
-            result(playerSelection, getComputerChoice, 0);
-            return 0;
-        }else{
-            result(playerSelection, getComputerChoice);
-        }
-    } else if(playerSelection === "SCISSORS"){
-        if(getComputerChoice === "ROCK"){
-            result(playerSelection, getComputerChoice, 0);
-            return 0;
-        }else if(getComputerChoice === "PAPER"){
-            result(playerSelection, getComputerChoice, 1);
-            return 1;
-        }else{
-            result(playerSelection, getComputerChoice);
-        }
-    }
-}
-
-//CREATE A FUNCTION FOR THE CALLS WHEN YOU WIN OR LOSE
-function result(player,computer,gameOutcome){
-    if(gameOutcome === 1){
-        console.log(`YOU WIN! ${player} BEATS ${computer}`);
-    }else if(gameOutcome === 0){
-        console.log(`YOU LOSE! ${computer} BEATS ${player}`);
-    }else if(gameOutcome === undefined){
-        console.log(`IT'S A TIE! ${player} EQUALS ${computer}`);
-    }
+    return playerSelection;
 }
 
 //CREATE A FUNCTION TO ADD ONE POINT TO CURRENT SCORE & RETURN IT
@@ -73,28 +34,76 @@ function addPoint(currScore){
 
 //CREATE A FUNCTION TO DISPLAY CURRENT SCORES
 function displayScore(playerScore, computerScore){
-    console.log(`YOUR SCORE: ${playerScore}`);
-    console.log(`COMPUTER SCORE: ${computerScore}`);
+    let scoreDisplay = document.querySelector(".score");
+
+    scoreDisplay.textContent = `YOUR SCORE: ${playerScore} COMPUTER SCORE: ${computerScore}`;
 }
 
-//PLAY A FIVE ROUND GAME OF ROCK, PAPER, SCISSORS
-function game(){
-    let currPlayScore = 0;
-    let currCompScore = 0;
+//RESET POINTS ONCE A PLAYER REACHES 5 POINTS
+function resetGame(){
+    let result = document.querySelector(".result");
 
-    for(round = 1; round <= 5; round++){
-
-        console.log(`ROUND: ${round}`);
-        let pointWon = playRound(playerSelection(),getComputerChoice());
-
-        if(pointWon === 1){
-            currPlayScore = addPoint(currPlayScore);
-        }else if(pointWon === 0){
-            currCompScore = addPoint(currCompScore);
-        }
-
-        displayScore(currPlayScore,currCompScore);
+    if(currPlayScore === 5){
+        result.textContent = "YOU ARE THE WINNER!";
+        currPlayScore = 0;
+        currCompScore = 0;
+    }else if(currCompScore === 5){
+        result.textContent = "THE COMPUTER IS THE WINNER!";
+        currPlayScore = 0;
+        currCompScore = 0;
     }
 }
 
-console.log(game());
+//CREATE A FUNCTION FOR THE CALLS WHEN YOU WIN OR LOSE
+function result(player,computer,gameOutcome){
+    let result = document.querySelector(".result");
+    
+    if(gameOutcome === 1){
+        result.textContent = `YOU WIN! ${player} BEATS ${computer}`;
+        currPlayScore = addPoint(currPlayScore);
+    }else if(gameOutcome === 0){
+        result.textContent = `YOU LOSE! ${computer} BEATS ${player}`;
+        currCompScore = addPoint(currCompScore);
+    }else if(gameOutcome === undefined){
+        result.textContent = `IT'S A TIE! ${player} EQUALS ${computer}`;
+    }
+    displayScore(currPlayScore,currCompScore);
+    resetGame();
+}
+
+//PLAY A SINGLE ROUND OF THE GAME
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "ROCK"){
+        if(computerSelection === "PAPER"){
+            result(playerSelection, computerSelection, 0);
+        }else if(computerSelection === "SCISSORS"){
+            result(playerSelection, computerSelection, 1);
+        }else{
+            result(playerSelection, computerSelection);
+        }
+    } else if(playerSelection === "PAPER"){
+        if(computerSelection === "ROCK"){
+            result(playerSelection, computerSelection, 1);
+        }else if(computerSelection === "SCISSORS"){
+            result(playerSelection, computerSelection, 0);
+        }else{
+            result(playerSelection, computerSelection);
+        }
+    } else if(playerSelection === "SCISSORS"){
+        if(computerSelection === "ROCK"){
+            result(playerSelection, computerSelection, 0);
+        }else if(computerSelection === "PAPER"){
+            result(playerSelection, computerSelection, 1);
+        }else{
+            result(playerSelection, computerSelection);
+        }
+    }
+}
+
+//LOOP THRU NODELIST OF BUTTONS & ISOLATE THE BUTTON THE PLAYER SELECTS USING EVENT LISTENER
+buttons.forEach(button => button.addEventListener("click", (event) => {
+    playRound(playerSelection(event), computerSelection());
+}));
+
+
+
