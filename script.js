d8377e176ed406cad6866d76b7ae52ba5e0e9261
indigoBill <117851497+indigoBill@users.noreply.github.com>
2023-08-27 22:13:57 -0400

    Clean up code

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-//CREATE REFERENCE TO BUTTON ELEMENTS IN HTML 
-let buttons = document.querySelectorAll(".player-select");
-
-//DECLARE VARIABLES FOR CURRENT SCORES FOR THE GAME
+const playerButtons = document.querySelectorAll(".player-select");
 let currPlayScore = 0;
 let currCompScore = 0;
 
-//GET COMPUTER CHOICE
 function computerSelection(){
-    let num = Math.ceil(Math.random() * 3);
+    let computerChoice = Math.ceil(Math.random() * 3);
 
-    if(num === 1){
+    if(computerChoice === 1){
         return "ROCK";
-    } else if(num === 2){
+    } else if(computerChoice === 2){
         return "PAPER";
-    } else if(num === 3){
+    } else if(computerChoice === 3){
         return "SCISSORS";
     } 
 }
 
-//GET PLAYER CHOICE FROM CLICK EVENT
 function playerSelection(event){
     let selectedButton = event.target.textContent;
     let playerSelection = selectedButton.toUpperCase();
@@ -26,36 +21,26 @@ function playerSelection(event){
     return playerSelection;
 }
 
-//CREATE A FUNCTION TO ADD ONE POINT TO CURRENT SCORE & RETURN IT
 function addPoint(currScore){
     currScore += 1;
     return currScore;
 }
 
-//CREATE A FUNCTION TO DISPLAY CURRENT SCORES
 function displayScore(playerScore, computerScore){
-    let playScore = document.querySelector(".player-score");
-    let compScore = document.querySelector(".computer-score");
+    const playScore = document.querySelector(".player-score");
+    const compScore = document.querySelector(".computer-score");
 
     playScore.textContent = `YOUR SCORE: ${playerScore}`;
     compScore.textContent = `COMPUTER SCORE: ${computerScore}`;
 }
 
-//RESET POINTS ONCE A PLAYER REACHES 5 POINTS
 function resetGame(){
-    let result = document.querySelector(".result");
+    const clearResult = document.querySelector(".result");
     const modal = document.querySelector(".modal");
     const modalText = document.querySelector(".modal-text");
     const playButton = document.querySelector(".play-button");
 
-    function closeModal(){
-        modal.style.display = "none";
-        currPlayScore = 0;
-        currCompScore = 0;
-        displayScore(currPlayScore,currCompScore);
-        result.textContent = "";
-    }
-
+    
     if(currPlayScore === 5){
         modal.style.display = "block";
         modalText.textContent = "YOU ARE THE WINNER!";
@@ -63,80 +48,104 @@ function resetGame(){
         modal.style.display = "block";
         modalText.textContent = "THE COMPUTER IS THE WINNER!";
         
+    }else{
+        return;
+    }
+
+    function closeModal(){
+        modal.style.display = "none";
+        currPlayScore = 0;
+        currCompScore = 0;
+        displayScore(currPlayScore,currCompScore);
+        clearResult.textContent = "";
     }
 
     playButton.addEventListener("click", closeModal);
 }
 
-//CREATE A FUNCTION TO DISPLAY THE RESULTS OF EACH PLAY
-function result(player,computer,gameOutcome){
-    let playResult = document.querySelector(".result");
+function resultOfRound(playerSelection,computerSelection,gameOutcome){
+    const playResult = document.querySelector(".result");
     
     if(gameOutcome === 1){
-        playResult.textContent = `YOU WIN! ${player} BEATS ${computer}`;
+        playResult.textContent = `YOU WIN! ${playerSelection} BEATS ${computerSelection}`;
         currPlayScore = addPoint(currPlayScore);
         playerScoreBoardSize();
     }else if(gameOutcome === 0){
-        playResult.textContent = `YOU LOSE! ${computer} BEATS ${player}`;
+        playResult.textContent = `YOU LOSE! ${computerSelection} BEATS ${playerSelection}`;
         currCompScore = addPoint(currCompScore);
         compScoreBoardSize();
     }else if(gameOutcome === undefined){
-        playResult.textContent = `IT'S A TIE! ${player} EQUALS ${computer}`;
+        playResult.textContent = `IT'S A TIE! ${playerSelection} EQUALS ${computerSelection}`;
     }
     displayScore(currPlayScore,currCompScore);
     resetGame();
     
 }
 
-//PLAY A SINGLE ROUND OF THE GAME
 function playRound(playerSelection, computerSelection){
     if(playerSelection === "ROCK"){
         if(computerSelection === "PAPER"){
-            result(playerSelection, computerSelection, 0);
+            resultOfRound(playerSelection, computerSelection, 0);
         }else if(computerSelection === "SCISSORS"){
-            result(playerSelection, computerSelection, 1);
+            resultOfRound(playerSelection, computerSelection, 1);
         }else{
-            result(playerSelection, computerSelection);
+            resultOfRound(playerSelection, computerSelection);
         }
     } else if(playerSelection === "PAPER"){
         if(computerSelection === "ROCK"){
-            result(playerSelection, computerSelection, 1);
+            resultOfRound(playerSelection, computerSelection, 1);
         }else if(computerSelection === "SCISSORS"){
-            result(playerSelection, computerSelection, 0);
+            resultOfRound(playerSelection, computerSelection, 0);
         }else{
-            result(playerSelection, computerSelection);
+            resultOfRound(playerSelection, computerSelection);
         }
     } else if(playerSelection === "SCISSORS"){
         if(computerSelection === "ROCK"){
-            result(playerSelection, computerSelection, 0);
+            resultOfRound(playerSelection, computerSelection, 0);
         }else if(computerSelection === "PAPER"){
-            result(playerSelection, computerSelection, 1);
+            resultOfRound(playerSelection, computerSelection, 1);
         }else{
-            result(playerSelection, computerSelection);
+            resultOfRound(playerSelection, computerSelection);
         }
     }
 }
 
-function mouseOver(e){
-    let mouseOver = e.target;
+function mouseOverButtons(event){
+    let mouseOver = event.target;
     mouseOver.style.backgroundColor = "rgb(236, 230, 199)";
 }
 
-function mouseOut(e){
-    let mouseOut = e.target;
+function mouseOutButtons(event){
+    let mouseOut = event.target;
     mouseOut.style.backgroundColor = "rgb(240, 231, 231)";
 }
 
-//LOOP THRU NODELIST OF BUTTONS & ISOLATE THE BUTTON THE PLAYER SELECTS USING EVENT LISTENER
-buttons.forEach(button => button.addEventListener("click", (event) => {
+playerButtons.forEach(button => button.addEventListener("mouseover", mouseOverButtons));
+playerButtons.forEach(button => button.addEventListener("mouseout", mouseOutButtons));
+
+function overPlayAgainButton(event){
+    let select = event.target;
+    select.classList.add("selectPlayAgain");
+}
+
+function outPlayAgainButton(event){
+    let select = event.target;
+    select.classList.remove("selectPlayAgain");
+}
+
+const playAgainButton = document.querySelector(".play-button");
+
+playAgainButton.addEventListener("mouseover", overPlayAgainButton);
+playAgainButton.addEventListener("mouseout", outPlayAgainButton);
+
+//LOOP THRU NODELIST OF playerButtons TO GET THE playerSelection TO PLAY A ROUND OF RPS
+playerButtons.forEach(button => button.addEventListener("click", (event) => {
     playRound(playerSelection(event), computerSelection());
 }));
 
-//EDIT STYLE OF BUTTONS WHEN THEY ARE HOVERED OVER
-buttons.forEach(button => button.addEventListener("mouseover", mouseOver));
-buttons.forEach(button => button.addEventListener("mouseout", mouseOut));
 
-//INCREASE SCOREBOARD SIZES FOR POINT INCREASE ANIMATION
+
+//INCREASE SCOREBOARD SIZES FOR MORE VISUAL EMPHASIS OF WHICH PLAYER GAINED A POINT
 function playerScoreBoardSize(){
     const playerBkgd = document.querySelector(".player-score");
     playerBkgd.classList.add("size-change");
@@ -147,15 +156,19 @@ function compScoreBoardSize(){
     compBkgd.classList.add("size-change");
 }
 
-//USE EVENT LISTENER TO REDUCE SCOREBOARD SIZES
+//USE EVENT LISTENER TO REDUCE SCOREBOARD SIZE OF THE ROUND WINNER TO COMPLETE THE SCORE INCREASE ANIMATION
 const scoreBkgds = document.querySelectorAll(".score-bkgd");
+
+function reduceScoreBkgd(event){
+    if(event.propertyName !== "transform") return;
+    this.classList.remove("size-change");
+}
 
 scoreBkgds.forEach(scoreBkgd => scoreBkgd.addEventListener("transitionend", reduceScoreBkgd));
 
-function reduceScoreBkgd(e){
-    if(e.propertyName !== "transform") return;
-    this.classList.remove("size-change");
-}
+
+
+
 
 
 
